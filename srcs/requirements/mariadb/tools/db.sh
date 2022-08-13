cab3a644665bb5032d5d4f7b16445a25942dc5b8
mafortin <mafortin@student.42quebec.com>
2022-08-13 12:13:02 -0400

    init db + user, need to check if statement

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -1,5 +1,34 @@
 #! /bin/bash
 
-mysql_install_db
+echo "start of mariadb script"
+SQL_DATA_DIR="/var/lib/mysql"
+DB_NAME="wordpress_db"
+DB_USER="mafortin"
+DB_PASSWORD='pass'
+DB_ROOT_PASSWORD='root'
+if [ ! -d "/run/mysqld" ]; then
+	echo "Creating mysql daemon directory + adding rights"
+	mkdir /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
+echo "Installing db"
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	echo "Installing db"
+	chown -R mysql:mysql /var/lib/mysql
+	mysql_install_db --user=mysql  --basedir=/usr --datadir=$SQL_DATA_DIR
+	service mysql start & sleep 5
+	mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+	echo "ROOT PASSWORD SET"
+	mysql -u root -p$DB_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;"
+	echo "DB CREATED"
+	mysql -u root -p$DB_ROOT_PASSWORD -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+	echo "USER CREATED"
+	mysql -u root -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+	echo "PRIVILEGES GRANTED"
+	mysql -u root -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+fi
+	#/usr/bin/mysqld --user=mysql --bootstart < /tmp/init.sql
+tail -f
 
-/etc/init.d/mysql start
+
+echo "end of mariadb script"
