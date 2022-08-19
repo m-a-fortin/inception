7982134e7b65e3bdc794b2aa458bf5f3c5565207
mafortin <mafortin@student.42quebec.com>
2022-08-19 10:17:40 -0400

    db done, nginx + open ssl setup done

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
-echo "start of mariadb script"
+echo "START OF INIT_DB SCRIPT"
 SQL_DATA_DIR="/var/lib/mysql"
 DB_NAME="wordpress_db"
 DB_USER="mafortin"
 DB_PASSWORD='pass'
 DB_ROOT_PASSWORD='root'
-if [ ! -d "/run/mysqld" ]; then
+if [ ! -d "./run_mysqld.tmp" ]; then
 	echo "Creating mysql daemon directory + adding rights"
 	mkdir /run/mysqld
 	chown -R mysql:mysql /run/mysqld
+	touch ./run_mysqld.tmp
 fi
-echo "Installing db"
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	echo "Installing db"
+if [ ! -d "./var_mysql.tmp" ]; then
+	echo "INSTALLING DB"
 	chown -R mysql:mysql /var/lib/mysql
 	mysql_install_db --user=mysql  --basedir=/usr --datadir=$SQL_DATA_DIR
 	service mysql start & sleep 5
@@ -26,9 +26,9 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql -u root -p$DB_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO ${DB_USER}@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 	echo "PRIVILEGES GRANTED"
 	mysql -u root -p$DB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+	touch ./var_mysql.tmp
+	killall mysqld
 fi
-	#/usr/bin/mysqld --user=mysql --bootstart < /tmp/init.sql
-tail -f
-
-
-echo "end of mariadb script"
+echo "MARIADB SETUP COMPLETED"
+echo "MARIADB SERVER LISTENING ON 3306"
+exec $@
